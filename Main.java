fa0e5e0b41d39f1a9b1c4508cfa99311b4ca6295
lucix17 <91908402+mihirx17@users.noreply.github.com>
2022-09-23 15:02:58 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void pairs_of_Array(int a[])
+    {
+        int current;
+        for (int i=0;i<a.length;i++)
+        {
+            current=a[i];
+            for (int j=i+1;j<a.length;j++)
+            {
+                System.out.print("("+current + a[j]+")" );
+            }
+            System.out.println("");
+        }
+    }
+    public static void main(String[] args)
+
+    {
+
+        int a[]={1,2,4,5};
+        pairs_of_Array(a);
+    }
+}
